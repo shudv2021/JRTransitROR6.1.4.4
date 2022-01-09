212193fe1e4559fb7502f4fe428e47dfd6e9ffa6
dmitry <shudv@yandex.ru>
2022-01-09 08:31:24 +0300

    add transit representation

--- app/controllers/transits_controller.rb
+++ app/controllers/transits_controller.rb
@@ -1,7 +1,41 @@
 class TransitsController < ApplicationController
 
-    def index
+  def index
     @transits = Transit.all
+  end
+
+  def new
+    @transit = Transit.new
+  end
+
+  def create
+    @transit = Transit.new(transit_params)
+    if @transit.save
+      redirect_to transits_path
+    else
+    render :new
     end
-    
+  end
+  
+  def edit
+    @transit = Transit.find_by(id: params[:id])
+  end
+
+  def update
+    @transit = Transit.find_by(id: params[:id])
+    if @transit.update(transit_params)
+      redirect_to transits_path
+    else
+    render :new
+    end
+  end
+  
+private
+
+  def transit_params
+    params.require(:transit).permit(:first_name, :second_name, :patronymic, :tel,
+                                    :email, :weight, :length, :width, :height, :point_of_departure,
+                                    :destination)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+resources :transits, only: [:index, :new, :create, :edit, :update, :show, :destory]
 
-  get '/transits', to: 'transits#index'
-
+  # get '/transits', to: 'transits#index'
+  # get '/transits/new', to: 'transits#new'
+  # post '/transits', to: 'transits#create'
+  # get '/transits/:id/edit', to: 'transits#edit'
+  
   root 'pages#index'
 
 end
